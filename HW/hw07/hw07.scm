a4f806c0042e8d8529f36128bac9b23c80efcdd4
ZZZzzzz430 <zzz832501@163.com>
2025-12-26 16:36:43 +0800

    hw07 done

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,21 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+;; 对数级递归的 pow（fast exponentiation）
+(define (pow base exp)
+  (cond
+    ((zero? exp) 1)
+    ((even? exp) (square (pow base (/ exp 2))))
+    (else (* base (square (pow base (/ (- exp 1) 2)))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
+
