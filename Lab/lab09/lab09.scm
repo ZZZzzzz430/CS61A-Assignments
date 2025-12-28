9545356c862b87241749a3e641cac496e8c53989
ZZZzzzz430 <zzz832501@163.com>
2025-12-28 12:28:48 +0800

    lab09 donw

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    (else 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (x) (+ x num))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  (if (< n 1)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1))))
+)
 
 (define (max a b)
   (if (> a b)
